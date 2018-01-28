94aba984fe947fdd483f1ff5b40f86acd73b34ee
Your Name <you@example.com>
2018-01-28 16:21:30 +0900

    Change IOU calculation with https://www.kaggle.com/aglotero/another-iou-metric/notebook Apply early stopping of training based on validation loss

--- main.py
+++ main.py
@@ -158,16 +158,81 @@ from keras import backend as K
 from keras.losses import binary_crossentropy
 
 
-def mean_iou(y_true, y_pred):
+# def mean_iou(y_true, y_pred):
+#   prec = []
+#   for t in np.arange(0.5, 1.0, 0.05):
+#     y_pred_ = tf.to_int32(y_pred > t)
+#     score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+#     K.get_session().run(tf.local_variables_initializer())
+#     with tf.control_dependencies([up_opt]):
+#       score = tf.identity(score)
+#     prec.append(score)
+#   return K.mean(K.stack(prec))
+
+def iou_metric(y_true_in, y_pred_in, print_table=False):
+  labels = label(y_true_in > 0.5)
+  y_pred = label(y_pred_in > 0.5)
+
+  true_objects = len(np.unique(labels))
+  pred_objects = len(np.unique(y_pred))
+
+  intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
+
+  # Compute areas (needed for finding the union between all objects)
+  area_true = np.histogram(labels, bins=true_objects)[0]
+  area_pred = np.histogram(y_pred, bins=pred_objects)[0]
+  area_true = np.expand_dims(area_true, -1)
+  area_pred = np.expand_dims(area_pred, 0)
+
+  # Compute union
+  union = area_true + area_pred - intersection
+
+  # Exclude background from the analysis
+  intersection = intersection[1:, 1:]
+  union = union[1:, 1:]
+  union[union == 0] = 1e-9
+
+  # Compute the intersection over union
+  iou = intersection / union
+
+  # Precision helper function
+  def precision_at(threshold, iou):
+    matches = iou > threshold
+    true_positives = np.sum(matches, axis=1) == 1  # Correct objects
+    false_positives = np.sum(matches, axis=0) == 0  # Missed objects
+    false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
+    tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+    return tp, fp, fn
+
+  # Loop over IoU thresholds
   prec = []
+  if print_table:
+    print("Thresh\tTP\tFP\tFN\tPrec.")
   for t in np.arange(0.5, 1.0, 0.05):
-    y_pred_ = tf.to_int32(y_pred > t)
-    score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
-    K.get_session().run(tf.local_variables_initializer())
-    with tf.control_dependencies([up_opt]):
-      score = tf.identity(score)
-    prec.append(score)
-  return K.mean(K.stack(prec))
+    tp, fp, fn = precision_at(t, iou)
+    if (tp + fp + fn) > 0:
+      p = tp / (tp + fp + fn)
+    else:
+      p = 0
+    if print_table:
+      print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
+    prec.append(p)
+
+  if print_table:
+    print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+  return np.mean(prec)
+
+def iou_metric_batch(y_true_in, y_pred_in):
+  batch_size = y_true_in.shape[0]
+  metric = []
+  for batch in range(batch_size):
+    value = iou_metric(y_true_in[batch], y_pred_in[batch])
+    metric.append(value)
+  return np.array(np.mean(metric), dtype=np.float32)
+
+def my_iou_metric(label, pred):
+  metric_value = tf.py_func(iou_metric_batch, [label, pred], tf.float32)
+  return metric_value
 
 
 def dice_coef(y_true, y_pred):
@@ -213,6 +278,9 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.models import load_model
+
 from models import *
 
 if __name__ == "__main__":
@@ -226,11 +294,20 @@ if __name__ == "__main__":
   train_generator, val_generator = generator(xtr, xval, ytr, yval, batch_size)
 
   model = Unet(img_size)
-  model.compile(optimizer='adam', loss=bce_dice_loss, metrics=[mean_iou])
+  # model.compile(optimizer='adam', loss=bce_dice_loss, metrics=[mean_iou])
+  model.compile(optimizer='adam', loss=bce_dice_loss, metrics=[my_iou_metric])
 
-  model.fit_generator(train_generator, steps_per_epoch=len(xtr) / 6, epochs=50,
-                      validation_data=val_generator, validation_steps=len(xval) / batch_size)
+  earlystopper = EarlyStopping(patience=10, verbose=1)
+  checkpointer = ModelCheckpoint('model_highest_val.h5', verbose=1, save_best_only=True)
+  model.fit_generator(train_generator, steps_per_epoch=len(xtr) / 6, epochs=100,
+                      validation_data=val_generator, validation_steps=len(xval) / batch_size,
+                      callbacks=[earlystopper, checkpointer])
+  # # Reduced number of epochs only for test
+  # model.fit_generator(train_generator, steps_per_epoch=len(xtr) / 6, epochs=10,
+  #                     validation_data=val_generator, validation_steps=len(xval) / batch_size)
 
+  model = load_model('model_highest_val.h5',
+                     custom_objects={'my_iou_metric': my_iou_metric, 'bce_dice_loss': bce_dice_loss})
   preds_test = model.predict(X_test, verbose=1)
 
   preds_test_upsampled = []
